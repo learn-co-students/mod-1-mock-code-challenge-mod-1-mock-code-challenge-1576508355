841459152c5e65b3be633be6c2be1a33522cbd22
Annie Liao <liaowow@gmail.com>
2019-12-16 10:59:10 -0500

    deliverables done

--- book.rb
+++ book.rb
@@ -1,1 +1,30 @@
+class Book
 
+    attr_reader :title, :word_count, :author
+
+    @@all = []
+
+    def initialize(title, word_count, author)
+        @title = title
+        @word_count = word_count
+        @author = author
+        @@all << self
+    end
+
+    # return the author instance who wrote this book
+    def author
+        @author
+    end
+
+    def title
+        @title
+    end
+
+    def word_count
+        @word_count
+    end
+
+    def self.all
+        @@all
+    end
+end
--- console.rb
+++ console.rb
@@ -2,6 +2,21 @@ require 'pry'
 require_relative './book'
 require_relative './author'
 
-binding.pry
+#Author.new(name)
+a1 = Author.new("Author 1")
+a2 = Author.new("Author 2")
+a3 = Author.new("Author 3")
+a4 = Author.new("Author 4")
 
+# Book.new(title, word_count, author)
+b1 = Book.new("Book 1", 33_000, a1)
+b2 = Book.new("Book 2", 5_000, a1)
+b3 = Book.new("Book 3", 888, a3)
+b4 = Book.new("Book 4", 533_000, a1)
+b5 = Book.new("Book 5", 7_046, a2)
+b6 = Book.new("Book 6", 888, a3)
+
+binding.pry
+puts "yay"
+# Book >- Author
 
