5d08cecbf567666131f95adf8747fe9492c3332d
Jeremy Perry <jeremy.m.perry@gmail.com>
2019-12-16 14:23:37 -0500

    WIP

--- author.rb
+++ author.rb
@@ -1,1 +1,23 @@
+class Author
 
+
+    @@all= []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def write_book(title, word_count)
+        Book.new(self, title, word_count)
+    end
+
+    def books
+        Book.all.select {|x| x.author == self}
+    end
+
+    def self.all
+        @@all
+    end
+
+end
--- console.rb
+++ console.rb
@@ -2,6 +2,16 @@ require 'pry'
 require_relative './book'
 require_relative './author'
 
+
+author_1 = Author.new("Stephen King")
+author_2 = Author.new("Dean Koontz")
+
+book1 = Book.new(author_1, "The Shining", 5026)
+book2 = Book.new(author_1, "The Tower Part:1", 10234)
+book3 = Book.new(author_1, "Doctor Who", 11234)
+book4 = Book.new(author_2, "Sole Survivor", 15000)
+book5 = Book.new(author_2, "Phantoms", 18000)
+book5 = Book.new(author_2, "Intensity", 25000)
 binding.pry
 
 
