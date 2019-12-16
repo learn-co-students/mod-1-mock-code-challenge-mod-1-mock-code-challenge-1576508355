1bb19be0aa392da891ef3a8ebb1f0187331eecd9
angelopoole <angelo.poole@outlook.com>
2019-12-16 11:42:00 -0500

    this is the progress i have at 45 minutes. im having issues with setting book objects up within author.

--- console.rb
+++ console.rb
@@ -2,6 +2,15 @@ require 'pry'
 require_relative './book'
 require_relative './author'
 
+# Author.new(name, books)
+jk = Author.new("jk","hp1")
+james = Author.new("James.p",["learn how to save these as book objects"])
+jk.write_book("jk","hpmysteries5",10)
+jk.write_book("jk","hpmysteries2",3555)
+# Book.new(author, title, wordcount)
+Am = Book.new(jk,"AM news with jk", 500)
+pm = Book.new(james,"pm is better with james", 5000)
+
 binding.pry
 
 
