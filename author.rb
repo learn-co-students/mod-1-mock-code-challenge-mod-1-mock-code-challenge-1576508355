3267e8ceeb71616e85655fb88d6782723c281e11
Jeremy Perry <jeremy.m.perry@gmail.com>
2019-12-16 15:20:13 -0500

    most deliverables

--- author.rb
+++ author.rb
@@ -12,8 +12,27 @@ class Author
         Book.new(self, title, word_count)
     end
 
-    def books
+    def books #works
         Book.all.select {|x| x.author == self}
+    end
+
+    def total_words
+        #gather all word_count instances for author
+        #old_way --- didnt work
+            #---total_count = 0
+            #---self.books.each {|x| x.total_count == word_count}
+            #---total_count += word_count
+        #add those numbers and produce a sum
+            #---total_count.sum(word_count)
+
+        #correct_way from review
+        total_count = books.map {|x| x.word_count}
+        total_count.sum
+    end
+
+    def self.most_words
+        #compare total words of different authors
+        
     end
 
     def self.all
