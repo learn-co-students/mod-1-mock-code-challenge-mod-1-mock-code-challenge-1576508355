61ac1d9b2dc7a6975aa7626945e8048f21c705ac
hylobates-lar <alison.v.quaglia@gmail.com>
2019-12-16 15:00:15 -0500

    last updates after review

--- author.rb
+++ author.rb
@@ -15,32 +15,30 @@ class Author
 
     def books
         Book.all.select do |book|
-            book.author == self
+            book.author == self 
         end
     end
 
     def write_book(title, word_count)
-        Book.new(self, title, word_count)
-         
+        Book.new(self, title, word_count)    
     end
 
     def total_words
         # should return the total number of words that author has written across all of their authored books. 
-        my_books = Book.all.select do |book|
-            book.author == self
-        end
+        total_words = 0
+        my_books = self.books 
+
         my_books.map do |book|
-            book.word_count
-        end
-        
+            total_words += book.word_count 
+        end  
+        total_words
     end
 
     def self.most_words
     # should return the author instance who has written the most words
-        sorted_books = Book.all.max_by do |book|
-            book.word_count
+        self.all.max_by do |author|
+            author.total_words
         end
-        sorted_books[0]
     end
 
 end
--- console.rb
+++ console.rb
@@ -2,14 +2,16 @@ require 'pry'
 require_relative './book'
 require_relative './author'
 
-happy_feet = Book.new("Ted", "Happy_Feet", 1000)
-hello_world = Book.new("Susan", "Hello_World", 1500)
-about_ducks = Book.new("Eva", "About Ducks", 3000)
-about_geese = Book.new("Eva", "About Geese", 3500)
 
 ted = Author.new("Ted")
 susan = Author.new("Susan")
 eva = Author.new("Eva")
+
+happy_feet = Book.new(ted, "Happy_Feet", 1000)
+hello_world = Book.new(susan, "Hello_World", 1500)
+about_ducks = Book.new(eva, "About Ducks", 3000)
+about_geese = Book.new(eva, "About Geese", 3500)
+
 
 
 
